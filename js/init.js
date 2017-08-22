01278b5e5cd32c731e344c8f3e8fe30d32efb4de
apreston <apreston@curbellmedical.com>
2017-08-21 22:10:21 -0400

    added cabins page for template. changed navbar to only expand current page.

--- js/init.js
+++ js/init.js
@@ -4,6 +4,12 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+
+  	$('.carousel').carousel();
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.slider').slider();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
